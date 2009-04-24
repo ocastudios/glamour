4aeef1de18ba01a2cae878cf3492956da022d4e1
nelson@ocastudios.com <>
2009-04-24 20:48:53 -0300

    panel

--- panel.py
+++ panel.py
@@ -7,18 +7,21 @@ class Panel():
         self.size = size
         self.background = background
         self.data = []
+
     def print_data(self):
-        for infor in self.data:
-            infor_surface = infor.font.render((infor.title+': '+str(infor.variable)),1,(0,0,0))
-            screen_surface.blit (infor_surface,(self.pos[0]+infor.pos[0],self.pos[1]+infor.pos[1]))
+        for i in self.data:
+            infor_surface = i.font.render((i.title+': '+str(i.variable)),1,(0,0,0))
+            screen_surface.blit (infor_surface,(self.pos[0]+i.pos[0],self.pos[1]+i.pos[1]))
+
+
 class Data():
     def __init__(self,title,variable,pos,level,index,fonte='FreeSans',size=40):
-        self.level = level        
-        self.title = title
-        self.index = index
-        self.pos = pos
-        self.font=pygame.font.Font('data/fonts/Domestic_Manners.ttf',size)
-        self.label = self.font.render((self.title+str(variable)),1,(0,0,0))
+        self.level  = level
+        self.title  = title
+        self.index  = index
+        self.pos    = pos
+        self.font   = pygame.font.Font('data/fonts/Domestic_Manners.ttf',size)
+        self.label  = self.font.render((self.title+str(variable)),1,(0,0,0))
         self.level.panel.insert(index,self)
     def update(self,variable):
         self.label = self.font.render((self.title+str(variable)),1,(0,0,0))
